e9cbd6421b5b19eb2e9a22fada63e670f92b53a8
PaulEl <pelliott67@comcast.net>
2014-11-15 13:27:11 -0800

    fullfilled assignment so pushing before experimenting

--- javascript/script.js
+++ javascript/script.js
@@ -19,5 +19,23 @@ function giveHint() {
 }
 
 
-
 //Function called "signUp", causes a prompt to appear and ask for user's email address, then prints to the "output-here" paragraph "Thanks for signing up, " and the entered email address. Should be triggered by clicking on the button with id of "sign-up".
+
+
+function signUp() {
+    var emailRegex = /\w+@(\w+\.)+[\w+]{2,3}$/;
+    var userEmail = prompt("Enter your email");
+    
+    if (userEmail == "") {
+        alert("We can sign you up without a valid email.");
+    }
+    else if ( !(emailRegex.test(userEmail)) ) {
+        alert("None of your shenanigans. Enter a VALID email.");
+    }
+    else {
+        document.getElementById("output-here").innerHTML = "Thanks for signing up, " + userEmail;
+    }
+}
+
+
+
